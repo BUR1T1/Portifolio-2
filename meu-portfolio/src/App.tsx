6b7613b33804c871d099acf88082f33dbf367c14
Otavio Ribeiro <otaviocaetano184@gmail.com>
2026-03-10 15:35:39 -0300

    feat: integração emailjs, whatsapp e configuração de deploy

--- meu-portfolio/src/App.tsx
+++ meu-portfolio/src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import Portfolio from "./pages/Portifolio";
 import Projetos from "./pages/Projetos/Projetos";
 import Timeline from "./pages/timeline/Timeline";
-import Header from "./components/Header/Header";
+
 import ContactBlock from "./components/mensageria/ContactBlock"
+import Header from "./components/Header/Header";
 
 function App() {
   return (
--- meu-portfolio/vite.config.ts
+++ meu-portfolio/vite.config.ts
@@ -10,4 +10,6 @@ export default defineConfig({
       },
     }),
   ],
+  // ADICIONE ESTA LINHA:
+  base: '/nome-do-seu-repositorio/', 
 })
